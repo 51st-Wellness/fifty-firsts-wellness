96262e57fd8bd372479336c6d2613a3feba1cdac
Prosper Coded <enweremproper@gmail.com>
2025-09-15 16:43:31 +0100

    update .gitignore to exclude http.js and format http.js for consistency

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 
 const http = () => {
-    let options = {
-        baseURL: import.meta.env.VITE_API_URL,
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            "Access-Control-Allow-Origin": "*",
-        },
-    };
-    if (localStorage.getItem("token")) {
-        options.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
-    }
-    return axios.create(options);
+  let options = {
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  };
+  if (localStorage.getItem("token")) {
+    options.headers["Authorization"] = `Bearer ${localStorage.getItem(
+      "token"
+    )}`;
+  }
+  return axios.create(options);
 };
 export default http;
